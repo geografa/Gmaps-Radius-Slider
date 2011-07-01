c6a2bfe6ee2747bd761320b920f574e5c2448379
geografa <rafamoustafah@gmail.com>
2011-07-01 13:56:26 -0700

    Initial commit of all files.

---
+++ distancewidget.js
@@ -0,0 +1,197 @@
+/**
+ * A distance widget that will display a circle that can be resized and will
+ * provide the radius in km.
+ *
+ * @param {google.maps.Map} map The map on which to attach the distance widget.
+ *
+ * @constructor
+ */
+function DistanceWidget(map) {
+  this.set('map', map);
+  this.set('position', map.getCenter());
+
+  var marker = new google.maps.Marker({
+    draggable: true,
+    title: 'Drag me!'
+  });
+
+  // Bind the marker map property to the DistanceWidget map property
+  marker.bindTo('map', this);
+
+  // Bind the marker position property to the DistanceWidget position
+  // property
+  marker.bindTo('position', this);
+
+	// Create a new radius widget
+	var radiusWidget = new RadiusWidget();
+
+	// Bind the radiusWidget map to the DistanceWidget map
+	radiusWidget.bindTo('map', this);
+
+	// Bind the radiusWidget center to the DistanceWidget position
+	radiusWidget.bindTo('center', this, 'position');
+	
+	// Bind to the radiusWidgets' distance property
+	this.bindTo('distance', radiusWidget);
+
+	// Bind to the radiusWidgets' bounds property
+	this.bindTo('bounds', radiusWidget);
+
+}
+
+DistanceWidget.prototype = new google.maps.MVCObject();
+
+/**
+ * A radius widget that add a circle to a map and centers on a marker.
+ *
+ * @constructor
+ */
+function RadiusWidget() {
+  var circle = new google.maps.Circle({
+    strokeWeight: 2
+  });
+
+  // Set the distance property value, default to 2km.
+  this.set('distance', 2);
+
+  // Bind the RadiusWidget bounds property to the circle bounds property.
+  this.bindTo('bounds', circle);
+
+  // Bind the circle center to the RadiusWidget center property
+  circle.bindTo('center', this);
+
+  // Bind the circle map to the RadiusWidget map
+  circle.bindTo('map', this);
+
+  // Bind the circle radius property to the RadiusWidget radius property
+  circle.bindTo('radius', this);
+
+	this.addSizer_();
+}
+RadiusWidget.prototype = new google.maps.MVCObject();
+
+/**
+ * Update the center of the circle and position the sizer back on the line.
+ *
+ * Position is bound to the DistanceWidget so this is expected to change when
+ * the position of the distance widget is changed.
+ */
+RadiusWidget.prototype.center_changed = function() {
+  var bounds = this.get('bounds');
+
+  // Bounds might not always be set so check that it exists first.
+  if (bounds) {
+    var lng = bounds.getNorthEast().lng();
+
+    // Put the sizer at center, right on the circle.
+    var position = new google.maps.LatLng(this.get('center').lat(), lng);
+    this.set('sizer_position', position);
+  }
+};
+
+/**
+ * Calculates the distance between two latlng locations in km.
+ * @see http://www.movable-type.co.uk/scripts/latlong.html
+ *
+ * @param {google.maps.LatLng} p1 The first lat lng point.
+ * @param {google.maps.LatLng} p2 The second lat lng point.
+ * @return {number} The distance between the two points in km.
+ * @private
+*/
+RadiusWidget.prototype.distanceBetweenPoints_ = function(p1, p2) {
+  if (!p1 || !p2) {
+    return 0;
+  }
+
+  var R = 6371; // Radius of the Earth in km
+  var dLat = (p2.lat() - p1.lat()) * Math.PI / 180;
+  var dLon = (p2.lng() - p1.lng()) * Math.PI / 180;
+  var a = Math.sin(dLat / 2) * Math.sin(dLat / 2) +
+    Math.cos(p1.lat() * Math.PI / 180) * Math.cos(p2.lat() * Math.PI / 180) *
+    Math.sin(dLon / 2) * Math.sin(dLon / 2);
+  var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+  var d = R * c;
+  return d;
+};
+
+
+/**
+ * Set the distance of the circle based on the position of the sizer.
+ */
+RadiusWidget.prototype.setDistance = function() {
+  // As the sizer is being dragged, its position changes.  Because the
+  // RadiusWidget's sizer_position is bound to the sizer's position, it will
+  // change as well.
+  var pos = this.get('sizer_position');
+  var center = this.get('center');
+  var distance = this.distanceBetweenPoints_(center, pos);
+
+  // Set the distance property for any objects that are bound to it
+  this.set('distance', distance);
+};
+
+
+/**
+ * Update the radius when the distance has changed.
+ */
+RadiusWidget.prototype.distance_changed = function() {
+  this.set('radius', this.get('distance') * 1000);
+};
+
+/**
+ * Add the sizer marker to the map.
+ *
+ * @private
+ */
+RadiusWidget.prototype.addSizer_ = function() {
+  var sizer = new google.maps.Marker({
+    draggable: true,
+    title: 'Resize me!'
+  });
+
+  sizer.bindTo('map', this);
+  sizer.bindTo('position', this, 'sizer_position');
+
+	var me = this;
+	google.maps.event.addListener(sizer, 'drag', function() {
+	  // Set the circle distance (radius)
+	  me.setDistance();
+	});
+};
+
+function displayInfo(widget) {
+  var info = document.getElementById('info');
+  info.innerHTML = 'Position: ' + widget.get('position') + ', distance: ' +
+    widget.get('distance') +
+		' km';
+}
+
+
+var tableid = 517214; //GISP data
+
+
+function init() {
+  var mapDiv = document.getElementById('map');
+  var map = new google.maps.Map(mapDiv, {
+    center: new google.maps.LatLng(45.522846311090994, -122.66002535814152),
+    zoom: 13,
+    mapTypeId: google.maps.MapTypeId.ROADMAP
+  });				
+	var distanceWidget = new DistanceWidget(map);
+	
+	google.maps.event.addListener(distanceWidget, 'distance_changed', function() {
+	  displayInfo(distanceWidget);
+	});
+
+	google.maps.event.addListener(distanceWidget, 'position_changed', function() {
+	  displayInfo(distanceWidget);
+	});
+	
+	layer = new google.maps.FusionTablesLayer(tableid, {
+    suppressInfoWindows: false
+  });
+  layer.setMap(map);
+  initializeSlider();  		
+}
+
+google.maps.event.addDomListener(window, 'load', init);
---
+++ slider.js
@@ -0,0 +1,59 @@
+goog.require('goog.ui.Dialog');
+goog.require('goog.ui.Slider');
+goog.require('goog.ui.Component');
+var sliderTimer, slider;
+
+// When a user clicks on a feature on the map, intercept the
+// click event and display the data in a modal dialog box.
+function displayData(mouseEvent) {
+  var dialog = new goog.ui.Dialog();
+  dialog.setTitle(mouseEvent.row['name'].value);
+  dialog.setContent(formatTableRowAsHtml(mouseEvent.row));
+  dialog.setButtonSet(goog.ui.Dialog.ButtonSet.OK);
+  dialog.setVisible(true);
+}
+
+// Produce some nicely formatted HTML using the raw row data exposed as
+// part of the feature click event.
+function formatTableRowAsHtml(row) {
+  var parts = [
+    '<center><img width="400px" height="200px" src="',
+    row['pic_link'].value,
+    '"></img><br>',
+    '<b>elevation: </b>',
+    row['min_elevation'].value,
+    ' to ',
+    row['max_elevation'].value,
+    ' feet</center>'
+  ];
+  return parts.join('');
+}
+
+// Create a Google Closure slider bar which can be used to select the
+// distance by which to filter the Fusion Tables data.
+function initializeSlider() {
+    var sliderElement = document.getElementById('slider');
+    slider = new goog.ui.Slider;
+    slider.decorate(sliderElement);
+    slider.addEventListener(goog.ui.Component.EventType.CHANGE, function() {
+      // Avoid updating the map too often by ignoring slider value changes
+      // that occur within 200mS of eachother.
+      if (sliderTimer) window.clearTimeout(sliderTimer);
+      sliderTimer = window.setTimeout(updateMap, 200);
+      document.getElementById("slider-value").innerHTML = slider.getValue() + " mi";
+    });
+    slider.setValue(25);
+}
+
+// Convert a slider value (0 - 100%) to an distance between 0 and 3000.
+function sliderValueToKm(value) {
+  return Math.round(value * 1.609344);
+}
+
+// Update the query used to filter Fusion Tables data using the
+// current value of the slider.
+function updateMap() {
+  radius = sliderValueToKm(slider.getValue());
+	// updateQuery = "SELECT City FROM " + tableid + " WHERE ST_INTERSECTS(City, CIRCLE(LATLNG(" + marker.getPosition().lat() + "," + marker.getPosition().lng() + ")," + radius + "))";
+	// layer.setQuery(updateQuery);
+}
